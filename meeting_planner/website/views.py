54bbcc9ea02d4bbb56b16f77c25f23fd987b419f
Prabal Deshar <prabaldeshar@gmail.com>
2022-06-26 23:18:59 +0545

    Add: detail page added and data passed from the views to detail page

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from datetime import datetime
+
+from meetings.models import Meeting
 # Create your views here.
 def welcome(request):
-    return render(request, "website/welcome.html", {"message": "This data was sent from the views to the template"})
+    return render(request, "website/welcome.html", {"num_meetings": Meeting.objects.count()})
 
 def date(request):
     return HttpResponse("This page was serverd at " + str(datetime.now()))
