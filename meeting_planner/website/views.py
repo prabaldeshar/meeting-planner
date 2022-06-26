4321d24f9855d8d6ecbcd9b913ca6871429923d8
Prabal Deshar <prabaldeshar@gmail.com>
2022-06-26 19:22:48 +0545

    Add: functions in views

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from datetime import datetime
 # Create your views here.
 def welcome(request):
     return HttpResponse("Welcome to the meeting planner")
+
+def date(request):
+    return HttpResponse("This page was serverd at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("This is the about page")
