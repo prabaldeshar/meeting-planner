092f4ce56331e75d146146aa56c108b45f2a3ea8
Prabal Deshar <prabaldeshar@gmail.com>
2022-07-02 23:20:13 +0545

    Add: Adding User Interaction with ModelFormns

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import redirect, render, get_object_or_404
+from django.forms import modelform_factory
 from .models import Meeting, Room
 
 # Create your views here.
@@ -11,5 +11,14 @@ def rooms(request):
     rooms = Room.objects.all()
     return render(request, "meetings/rooms.html", {"rooms": rooms})
 
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
 def new(request):
-    return render(request, "meetings/new.html")
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
