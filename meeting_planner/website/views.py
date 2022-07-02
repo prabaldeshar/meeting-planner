4644ae16867a26c49323494311d06a72a2615b2c
Prabal Deshar <prabaldeshar@gmail.com>
2022-07-02 20:25:38 +0545

    Add: Urls and Link Building

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from meetings.models import Meeting
 # Create your views here.
 def welcome(request):
-    return render(request, "website/welcome.html", {"num_meetings": Meeting.objects.count()})
+    return render(request, "website/welcome.html", {"meetings": Meeting.objects.all()})
 
 def date(request):
     return HttpResponse("This page was serverd at " + str(datetime.now()))
