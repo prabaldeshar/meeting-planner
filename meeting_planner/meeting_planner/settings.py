517f0ff661244c5cd380cb67b830487c40e383c3
Prabal Deshar <prabaldeshar@gmail.com>
2022-07-03 13:03:51 +0545

    Upadate: Secret key used from Environment variable

--- meeting_planner/meeting_planner/settings.py
+++ meeting_planner/meeting_planner/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kx!buqun++&2kx(($@hx)gcwfdh7@8$tla72yl5eo6ewm)xb$%'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["3.110.104.78"]
 
 
 # Application definition
